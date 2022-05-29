50701a0d1b73ee97adbd3904d5840acceaa3b8fd
Zharkova <62789319+Olga-Zharkova@users.noreply.github.com>
2022-05-29 23:57:26 +0300

    2 урок

---
+++ lib/secondLesson/2.dart
@@ -0,0 +1,44 @@
+
+
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class CarouselSliderWidget extends StatefulWidget {
+  const CarouselSliderWidget({Key? key}) : super(key: key);
+
+  @override
+  State<CarouselSliderWidget> createState() => _CarouselSliderWidgetState();
+}
+
+class _CarouselSliderWidgetState extends State<CarouselSliderWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue.shade200,
+      child: CarouselSlider(
+        items: [
+          SvgPicture.asset(
+              'assets/images/svg1.svg',
+              color: Colors.red,
+              semanticsLabel: 'A red up arrow'
+          ),
+          Image.asset('assets/images/image1.jpg'),
+          SvgPicture.asset('assets/images/svg2.svg'),
+          Image.network('https://2sotki.ru/wp-content/uploads/7/e/7/7e7006c676d5c9d52209cdcae884f793.jpe'),
+        ],
+        options: CarouselOptions(
+          height: 200.0,
+          enlargeCenterPage: true,
+          autoPlay: true,
+          aspectRatio: 16/9,
+          autoPlayCurve: Curves.fastOutSlowIn,
+          enableInfiniteScroll: true,
+          autoPlayInterval: const Duration(milliseconds: 200),
+          autoPlayAnimationDuration: const Duration(milliseconds: 800),
+          viewportFraction: 0.9,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/secondLesson/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '1.dart';
+import '2.dart';
+
+
+void main() {
+
+  runApp(const MaterialApp(
+    //первое задание
+    //home: ImageSVG()
+    //второе задание
+    home: CarouselSliderWidget()
+    //третье задание
+    ),
+  );
+}
