6a0d3d6a8a189ef6f8ec2133d12fcde02518e2fa
Zharkova <62789319+Olga-Zharkova@users.noreply.github.com>
2022-06-06 14:52:39 +0300

    myapp

--- second_lesson/lib/main.dart
+++ second_lesson/lib/main.dart
@@ -1,27 +1,8 @@
 import 'package:flutter/material.dart';
-import 'carousel_slider_widget.dart';
-import 'image_svg.dart';
+import 'package:untitled/my_app.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text(
-                'Hello!',
-                style: TextStyle(fontFamily: 'TiroDevanagariSanskrit'),
-              ),
-            ),
-            body: ListView(
-              children: const [
-                Padding(padding: EdgeInsets.only(top: 5)),
-                Text('Первое задание'),
-                ImageSVG(),
-                Padding(padding: EdgeInsets.only(top: 5)),
-                Text('Второе задание'),
-                CarouselSliderWidget(),
-                Padding(padding: EdgeInsets.only(top: 5)),
-              ],
-            ))),
+      const MyApp()
   );
 }
